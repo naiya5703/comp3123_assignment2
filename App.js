8c3254b997c5091ef13c53fb8131ac357ba58886
naiya5703 <156104698+naiya5703@users.noreply.github.com>
2024-12-01 23:51:16 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import EmployeeList from './pages/EmployeeList';
+import EmployeeForm from './pages/EmployeeForm';
+import './App.css';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/employees" element={<EmployeeList />} />
+        <Route path="/add-employee" element={<EmployeeForm />} />
+        <Route path="/edit-employee/:id" element={<EmployeeForm />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
